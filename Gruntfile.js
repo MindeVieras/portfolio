be94c7435b9cb0ca68e4da4cc8356c59190049f2
MindeVieras <wieraz21@gmail.com>
2017-10-29 20:16:57 +0000

    aws deploy done

--- Gruntfile.js
+++ Gruntfile.js
@@ -72,6 +72,66 @@ module.exports = function(grunt) {
                 }
             }
         },
+        imagemin: {
+            dynamic: {            
+                options: {
+                    optimizationLevel: 7,
+                    svgoPlugins: [{removeViewBox: false}],
+                    // use: [mozjpeg()] // Example plugin usage
+                },
+                files: [{
+                    expand: true,
+                    cwd: './src/images/',
+                    src: ['**/*.{png,jpg,gif}'],
+                    dest: './dist/images/'
+                }]
+            }
+        },
+        json_minification: {
+            target: {
+                files: [{
+                    expand: true,
+                    cwd: './src/models/',
+                    src: ['**/*.json'],
+                    dest: './dist/models/'
+                }]
+            }
+        },
+        copy: {
+            main: {
+                files: [
+                    {expand: true, cwd: './src/cv/', src: ['**'], dest: './dist/cv/'},
+                ],
+            },
+        },
+        aws: grunt.file.readJSON('./aws-keys.json'),
+        aws_s3: {
+            options: {
+                accessKeyId: '<%= aws.AWSAccessKeyId %>',
+                secretAccessKey: '<%= aws.AWSSecretKey %>',
+                region: 'eu-west-1',
+                uploadConcurrency: 5,
+                downloadConcurrency: 5
+            },
+            prod: {
+                options: {
+                    bucket: 'mindelis.com',
+                    access: 'public-read'
+                    // params: {
+                    //     ContentEncoding: 'gzip'
+                    // }
+                },
+                files: [{
+                    expand: true,
+                    cwd: './dist/',
+                    src: ['**'],
+                    dest: '/',
+                    params: {
+                        CacheControl: 'max-age=2629746'
+                    }
+                }]
+            },
+        },
         watch: {
             html: {
                 files: ['./src/**/*.html']
@@ -99,7 +159,11 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-json-minification');
+    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-aws-s3');
     
     // Task definition
     grunt.registerTask('default', [
@@ -108,11 +172,25 @@ module.exports = function(grunt) {
         'watch'
     ]);
     grunt.registerTask('build', [
+        'imagemin',
+        'json_minification',
+        'copy',
         'htmlmin:prod',
         'sass',
         'cssmin',
         'concat',
         'uglify'
+    ]);
+    grunt.registerTask('deploy', [
+        // 'imagemin',
+        // 'json_minification',
+        // 'copy',
+        // 'htmlmin:prod',
+        // 'sass',
+        // 'cssmin',
+        // 'concat',
+        // 'uglify',
+        'aws_s3'
     ]);
 
 };
