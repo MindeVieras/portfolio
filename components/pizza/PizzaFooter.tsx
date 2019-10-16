88173dc0906ed50e4b2731a4f540d315f12ad317
MindeVieras <padricam@coventry.ac.uk>
2019-10-16 20:44:47 +0100

    some components converted to functional.

--- components/pizza/PizzaFooter.tsx
+++ components/pizza/PizzaFooter.tsx
@@ -6,33 +6,31 @@ interface FooterProps {
   links: PizzaFooterLinkProps[];
 }
 
-class PizzaFooter extends Component<FooterProps> {
-  render() {
-    const { links } = this.props;
+const PizzaFooter = (props: FooterProps): JSX.Element => {
+  const { links } = props;
 
-    const footerLinks = links
-      // Sort array by link weight.
-      .sort((a, b) => a.weight - b.weight)
-      // Then return all links as PizzaFooterLink component.
-      .map((l, i) => <PizzaFooterLink key={i} {...l} />);
+  const footerLinks = links
+    // Sort array by link weight.
+    .sort((a, b) => a.weight - b.weight)
+    // Then return all links as PizzaFooterLink component.
+    .map((l, i) => <PizzaFooterLink key={i} {...l} />);
 
-    return (
-      <Fragment>
-        <footer>{footerLinks}</footer>
+  return (
+    <Fragment>
+      <footer>{footerLinks}</footer>
 
-        <style jsx>{`
-          footer {
-            display: flex;
-            justify-content: center;
-            position: absolute;
-            bottom: 8px;
-            left: 0;
-            width: 100%;
-          }
-        `}</style>
-      </Fragment>
-    );
-  }
-}
+      <style jsx>{`
+        footer {
+          display: flex;
+          justify-content: center;
+          position: absolute;
+          bottom: 8px;
+          left: 0;
+          width: 100%;
+        }
+      `}</style>
+    </Fragment>
+  );
+};
 
 export default PizzaFooter;
--- components/pizza/PizzaFooterLink.tsx
+++ components/pizza/PizzaFooterLink.tsx
@@ -10,41 +10,39 @@ export interface PizzaFooterLinkProps {
   weight: number;
 }
 
-class PizzaFooterIcon extends Component<PizzaFooterLinkProps> {
-  render() {
-    const { title, href, icon, target } = this.props;
+const PizzaFooterIcon = (props: PizzaFooterLinkProps): JSX.Element => {
+  const { title, href, icon, target } = props;
 
-    return (
-      <Fragment>
-        <a href={href} target={target} title={title} className="footer-link">
-          <div className="icon">
-            <Icon icon={icon} />
-          </div>
-        </a>
+  return (
+    <Fragment>
+      <a href={href} target={target} title={title} className="footer-link">
+        <div className="icon">
+          <Icon icon={icon} />
+        </div>
+      </a>
 
-        <style jsx>{`
-          a {
-            margin: 0 10px;
+      <style jsx>{`
+        a {
+          margin: 0 10px;
+        }
+        a:focus {
+          outline: 0;
+        }
+        a:hover .icon,
+        a:focus .icon {
+            color: rgba(200,200,200,.9);
           }
-          a:focus {
-            outline: 0;
-          }
-          a:hover .icon,
-          a:focus .icon {
-              color: rgba(200,200,200,.9);
-            }
-          }
-          .icon {
-            font-size: 48px;
-            color: rgba(204,204,204,.5);
-            line-height: 1;
-            cursor: pointer;
-            transition: all 0.25s ease;
-          }
-        `}</style>
-      </Fragment>
-    );
-  }
-}
+        }
+        .icon {
+          font-size: 48px;
+          color: rgba(204,204,204,.5);
+          line-height: 1;
+          cursor: pointer;
+          transition: all 0.25s ease;
+        }
+      `}</style>
+    </Fragment>
+  );
+};
 
 export default PizzaFooterIcon;
